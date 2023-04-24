ad3eee16bafd44d9cc320010c5e57aab39ae9528
Jack <jackforez@gmail.com>
2023-04-24 23:13:52 +0700

    v

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -6,11 +6,39 @@ import Dashboard from "./Dashboard";
 import Bill from "./Bill";
 import Cost from "./Cost";
 import Setting from "./Setting";
+import { AntDesign } from "@expo/vector-icons";
 const Tab = createBottomTabNavigator();
 const Login = ({ navigation }) => {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Dashboard" component={Dashboard} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          position: "absolute",
+          bottom: 20,
+          marginRight: 20,
+          marginLeft: 20,
+          justifyContent: "center",
+          alignItems: "center",
+          borderRadius: 20,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Dashboard"
+        component={Dashboard}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View className="justify-center items-center">
+              <AntDesign
+                name="home"
+                size={24}
+                color={focused ? "red" : "blue"}
+              />
+            </View>
+          ),
+        }}
+      />
       <Tab.Screen name="Bill" component={Bill} />
       <Tab.Screen name="Cost" component={Cost} />
       <Tab.Screen name="Setting" component={Setting} />
