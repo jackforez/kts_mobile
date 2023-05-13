4b28d148ebea2fdb4aca9fa5db5c110f81c73850
JackForez <jackforez@gmail.com>
2023-05-13 21:43:43 +0700

    config redux. v

--- src/screens/Setting.jsx
+++ src/screens/Setting.jsx
@@ -1,10 +1,13 @@
 import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
+import { TouchableOpacity } from "react-native";
 
 const Setting = () => {
   return (
     <SafeAreaView className="flex-1 items-center justify-center">
-      <Text>Setting</Text>
+      <TouchableOpacity className="p-3 rounded-md border border-indigo-900">
+        <Text className="">Đăng xuất</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
