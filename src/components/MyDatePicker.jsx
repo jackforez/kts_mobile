02376b73ae038be33de167bb1a298b8edb7d06be
JackForez <jackforez@gmail.com>
2023-07-25 16:21:41 +0700

    v

---
+++ src/components/MyDatePicker.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const MyDatePicker = () => {
+  return (
+    <View>
+      <Text className="text-red-500">MyDatePicker</Text>
+    </View>
+  );
+};
+
+export default MyDatePicker;
